cc6ad16e4fd0bc3e5c7fa8baff6de55f4ce67a7a
Swei99 <weilun513@gmail.com>
2025-08-21 16:37:42 +0800

    Add Rename, Greet, Exit into main

---
+++ src/main/java/weiwei.java
@@ -0,0 +1,15 @@
+public class weiwei {
+    public static void main(String[] args) {
+        String logo =
+        "__        __   _____    ___   __        __   _____    ___   \n" +
+        "\\ \\      / /  | ____|  |_ _|  \\ \\      / /  | ____|  |_ _|  \n" +
+        " \\ \\ /\\ / /   |  _|     | |    \\ \\ /\\ / /   |  _|     | |  \n" +
+        "  \\ V  V /    | |___    | |     \\ V  V /    | |___    | |   \n" +
+        "   \\_/\\_/     |_____|  |___|     \\_/\\_/     |_____|  |___|  \n" +
+        "___________________________________________________________\n";
+
+        System.out.println("Hello im \n" + logo + "\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
